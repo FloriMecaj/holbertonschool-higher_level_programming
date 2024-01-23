c19b3cad2f8147e894ec1b9e500f2b42bf44c5dc
FloriMecaj <florimecaj31@gmail.com>
2024-01-23 11:41:32 +0100

    islower

--- python-if_else_loops_functions/7-islower.py
+++ python-if_else_loops_functions/7-islower.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
-def is_lower(c):
-    u 
+def islower(c):
+    while True:
+        if ord(c) in range(ord('a'), ord('z') + 1):
+            return True
+        else:
+            return False
