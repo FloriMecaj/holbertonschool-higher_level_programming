bf617c644a9978c224122dae8786faa68a7def0d
FloriMecaj <florimecaj31@gmail.com>
2024-01-29 11:17:38 +0100

    new matrix**2

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -7,16 +7,3 @@ def square_matrix_simple(matrix=[]):
             new_list.append(element**2)
         new_matrix.append(new_list)
     return new_matrix
-
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-new_matrix = square_matrix_simple(matrix)
-print(new_matrix)
-print(matrix)
