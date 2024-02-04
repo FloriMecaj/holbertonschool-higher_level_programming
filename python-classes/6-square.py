263460b4eefef7425bdf28a47be4d32ff746c702
FloriMecaj <florimecaj31@gmail.com>
2024-02-04 18:52:00 +0100

    coordinates of a square

---
+++ python-classes/6-square.py
@@ -0,0 +1,50 @@
+#!/usr/bin/python3
+'''
+    We will create a class named Square
+'''
+
+
+class Square:
+    '''
+        Size must be an integer and greater than 0
+    '''
+    def __init__(self, size=0, position=(0, 0)):
+        self.size = size
+        self.position = position
+
+    @property
+    def size(self):
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        if not isinstance(value, tuple) or len(value) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        elif not all(isinstance(i, int) and i >= 0 for i in value):
+            raise ValueError("position must be a tuple of 2 positive integers")
+        else:
+            self.__position = value
+
+    def area(self):
+        return self.__size ** 2
+
+    def my_print(self):
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.__position[1]):
+                print()
+            for i in range(self.__size):
+                print(" " * self.__position[0] + "#" * self.__size)
