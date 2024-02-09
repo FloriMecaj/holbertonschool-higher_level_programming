a30ed56e603d2d3ffbe0d30acf3001218371b2b7
FloriMecaj <florimecaj31@gmail.com>
2024-02-09 11:28:09 +0100

    inherits form

---
+++ python-inheritance/4-inherits_from.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Only sub class of"""
+
+
+def inherits_from(obj, a_class):
+    """inherits from a class"""
+    if obj is isinstance(obj, a_class):
+        return True
+    return False
