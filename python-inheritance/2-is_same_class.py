0329a47a0b82afe481b8fd263547f1806b11f464
FloriMecaj <florimecaj31@gmail.com>
2024-02-09 09:45:29 +0100

    test

--- python-inheritance/2-is_same_class.py
+++ python-inheritance/2-is_same_class.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 '''
-is_smae_class
+Exact same object
 '''
+
+
 def is_same_class(obj, a_class):
+    '''
+    def is same class
+    '''
     if not isinstance(obj, a_class):
         return True
     return False
--- python-inheritance/3-is_kind_of_class.py
+++ python-inheritance/3-is_kind_of_class.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 '''
-is_kind_of_class
+Same class or inherit from
 '''
 
 
 def is_kind_of_class(obj, a_class):
-        return isinstance(obj, a_class)
+    '''
+    def is kind of class
+    '''
+    return isinstance(obj, a_class)
