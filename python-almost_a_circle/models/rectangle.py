9511cad7fc456cac07f7ff88145e6e672f4eb75e
FloriMecaj <florimecaj31@gmail.com>
2024-02-14 09:04:03 +0100

    display 1

--- python-almost_a_circle/models/rectangle.py
+++ python-almost_a_circle/models/rectangle.py
@@ -71,8 +71,8 @@ class Rectangle(Base):
     def display(self):
         """display #1"""
         print("\n" * self.y, end="")
-        for i in range(self.height):
-            print(" " * self.x, end=" ")
+        for _ in range(self.height):
+            print(" " * self.x, end="")
             print("#" * self.width)
 
     def __str__(self):
