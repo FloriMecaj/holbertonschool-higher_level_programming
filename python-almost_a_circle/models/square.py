b1bd541fea9bf5b1c2d7db2d5dd84c3bb16396b6
FloriMecaj <florimecaj31@gmail.com>
2024-02-14 09:56:45 +0100

    square get n set for size

--- python-almost_a_circle/models/square.py
+++ python-almost_a_circle/models/square.py
@@ -11,6 +11,15 @@ class Square(Rectangle):
         """super method"""
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        self.width = value
+        self.height = value
+
     def __str__(self):
         """__str__"""
         return (f"[Square] ({self.id}) {self.x}/{self.y} - {self.width}")
