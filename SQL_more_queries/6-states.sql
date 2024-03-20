2c68b1198a7dd3fc9a73dbd827f54f44e1df2376
FloriMecaj <florimecaj31@gmail.com>
2024-03-20 11:32:21 +0100

    unique & primary key ID

--- SQL_more_queries/6-states.sql
+++ SQL_more_queries/6-states.sql
@@ -1,6 +1,6 @@
 -- script that creates the database hbtn_0d_usa and the table states
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-USE DATABASE;
+USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
 	id int UNIQUE AUTO_INCREMENT NOT NULL,
 	name varchar(256) NOT NULL,
